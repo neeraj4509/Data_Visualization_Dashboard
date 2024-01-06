39a966181824b181c1111174193e83d63f4524d8
neeraj4509 <87565643+neeraj4509@users.noreply.github.com>
2024-01-06 18:06:59 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,43 +1,39 @@
 // signup.js
+import {createUserWithEmailAndPassword, updateProfile } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 
 function signUp() {
-  // Fetch values from the form
+
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const confirmPassword = document.getElementById('confirmPassword').value;
-  console.log("signUp")
-  // Check if passwords match
+
   if (password !== confirmPassword) {
-    alert("Passwords do not match");
+    alert("Passwords do not match.");
     return;
   }
 
-  // Create an object with user data
-  const userData = {
-    name: name,
-    email: email,
-    password: password
-  };
+  createUserWithEmailAndPassword(window.firebaseAuth,email, password)
+    .then((userCredential) => {
+      // User created successfully
+      return updateProfile(userCredential.user,{
+        displayName: name
+      });
+    })
+    .then(() => {
+      // Profile updated successfully
+      alert("User registered successfully.");
 
-  // Make a POST request to the server
-  fetch('/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userData)
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Handle the response from the server
-    alert(data.message);
-
-    
-    // Redirect to login page or perform other actions as needed
-  })
-  .catch(error => {
-    console.error('Error during signup:', error);
-    alert('Error during signup. Please try again.');
-  });
+      // Redirect to login page or another page
+      window.location.href = 'login.html'; // Adjust the redirection as needed
+    })
+    .catch((error) => {
+      // Handle errors here
+      console.error("Error during signup:", error);
+      alert("Error during signup. " + error.message);
+    });
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('signupButton').addEventListener('click', signUp);
+});
